0c910e1a1ede4b3557c5a8dcead51df12b5e693d
Daniel Barrett <danielbarrett@daniels-macbook-pro.local>
2016-04-17 16:53:58 -0400

    Initial Commit

---
+++ tea/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  tea
+//
+//  Created by Daniel Barrett on 4/17/16.
+//  Copyright © 2016 Daniel Barrett. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
