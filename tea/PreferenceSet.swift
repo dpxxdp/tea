663670837e6c2215cca67234b98c4c5fe119b42f
Daniel Barrett <danielbarrett@Daniels-MacBook-Pro.local>
2016-04-19 19:46:45 -0400

    preference class and menu item

---
+++ tea/PreferenceSet.swift
@@ -0,0 +1,14 @@
+//
+//  Preferences.swift
+//  tea
+//
+//  Created by Daniel Barrett on 4/19/16.
+//  Copyright © 2016 Daniel Barrett. All rights reserved.
+//
+
+import Foundation
+
+class PreferenceSet {
+
+    
+}
